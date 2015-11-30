bfdfbee907adfcbbcaffbbff76ec580baeaa3303
Edward Springer <ehs0329@gmail.com>
2015-11-29 21:51:13 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to edthirdtheword.com!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Welcome to edthirdtheword.com!</h1>
+<p>If you see this page, my nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>I will be adding much more to this site in the future. My plan is to use
+it to showcase the projects that I have worked on and progress for current
+projects. Any available sources, including this website, will be on my
+<a href="http://github.com/ehs0329">github page</a>.</p>
+
+<p>For a basic list of planned site features, you may refer to
+<a href="plans.php">future plans</a>.</p>
+
+</body>
+</html>
---
+++ plans.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<title>Future Plans</title>
+<h1>Future Plans</h1>
+<ul>
+    <li>Make site more functional with navbar/tabs/etc.</li>
+    <li>Style site with CSS</li>
+    <li>Build admin tools to make it easy to add new content</li>
+</ul>
+</html>
